e60795b0b4bb0e18cdb41160fc4fc4b15a4a54bc
Justin McMillion <79409949+jmcmillion89@users.noreply.github.com>
2024-01-21 17:24:17 -0500

    add script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function getComputerChoice() {
+    const hand = ['Rock', 'Paper', 'Scissors']
+    return hand[(Math.floor(Math.random() * hand.length))];
+}
+
+function playRound(playerSelection, computerSelection) {
+    
+}
