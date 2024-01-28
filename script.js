45523c19f5800e5694327dccbb63ed9d44ca9ff1
Justin McMillion <79409949+jmcmillion89@users.noreply.github.com>
2024-01-28 09:21:03 -0500

    add buttons

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-const computerSelection = getComputerChoice();
-let playerSelection = prompt('Choose Rock, Paper, or Scissors').toLowerCase()
+const buttons = document.querySelectorAll('input')
+
+buttons.forEach(button => {
+    button.addEventListener('click', function(){
+        playRound(button.value)
+    })
+})
 
 function getComputerChoice() {
-    const hand = ['rock', 'paper', 'scissors']
+    const hand = ['Rock', 'Paper', 'Scissors']
     return hand[(Math.floor(Math.random() * hand.length))];
 }
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return 'You Win! Rock beats Scissors.'
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+
+
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice();
+    if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+        return 'You Win! Rock beats Scissors.';
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
         return 'You Win! Scissors beats Paper.'
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
         return 'You Win! Paper beats Rock.'
     } else if (playerSelection === computerSelection) {
         return `It's a tie!`
     } else {
-        return `You Lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}.`
-    }
-}
-
-
-
-
-console.log(playRound(playerSelection, computerSelection));
+        return `You Lose! ${computerSelection} beats ${playerSelection}.`
+    }}
